c40d2b4b52700e8d78392858428c0f986115644c
Unuldur <julien.defiolles@laposte.net>
2017-11-29 18:50:59 +0100

    first commit

---
+++ app/src/main/java/com/unuldur/uminc/AccueilFragment.java
@@ -0,0 +1,42 @@
+package com.unuldur.uminc;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AccueilFragment extends Fragment {
+
+    public AccueilFragment() {
+        // Required empty public constructor
+    }
+
+    public static AccueilFragment newInstance() {
+       return new AccueilFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accueil, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Accueil");
+    }
+
+
+
+}
---
+++ app/src/main/java/com/unuldur/uminc/CalendarFragment.java
@@ -0,0 +1,54 @@
+package com.unuldur.uminc;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CalendarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalendarFragment extends Fragment {
+
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment CalendarFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CalendarFragment newInstance() {
+        return new CalendarFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Calendrier");
+    }
+
+}
